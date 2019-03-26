3f312b118341328eaa3dbba3b5f302a3080fc71a
Nicholas Eubanks <nick-2013@live.com>
2019-03-26 16:09:44 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  nice
 end
 
 def last_evaluated_value
